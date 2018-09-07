7a81cf5d6486ed55794d7c412a9227d30fa42f2b
Susruth Nadimpalli <susruth@republicprotocol.com>
2018-09-07 17:04:58 +1000

    Add updated files

--- packages/lib/networks/src/lib/networks.ts
+++ packages/lib/networks/src/lib/networks.ts
@@ -49,12 +49,16 @@ const falcon: NetworkData = {
     chain: "kovan",
     addresses: OrderedMap({
         [Category.Republic]: OrderedMap({
+            DarknodeRegistryStore: { address: "0x90e12af860038a4b45ddacf99751331376e5bfbb", version: ""},
             DarknodeRegistry: { address: "0xdaa8c30af85070506f641e456afdb84d4ba972bd", version: "" },
+            SettlementRegistry: { address: "0x6246ff83ddef23d9509ba80aa3ee650ab0321f0b", version: ""},
             Orderbook: { address: "0x592d16f8c5fa8f1e074ab3c2cd1acd087adcdc0b", version: "" },
             DarknodeRewardVault: { address: "0x401e7d7ce6f51ea1a8d4f582413e2fabda68daa8", version: "" },
+            DarknodeSlasher: { address: "0x71ec5f4558e87d6afb5c5ff0b4bdd058d62ed3d1", version: "" },
         }),
         [Category.RenEx]: OrderedMap({
             RenExBalances: { address: "0xb3e632943fa995fc75692e46b62383be49cddbc4", version: "" },
+            RenExBrokerVerifier: { address: "0xb6a95aed1588be477981dcdeacd13776570ecb3d", version: "" },
             RenExSettlement: { address: "0xbe936cb23dd9a84e4d9358810f7f275e93ccd770", version: "" },
             RenExTokens: { address: "0x9a898c8148131ef189b1c8575692376403780325", version: "" },
             RenExAtomicInfo: { address: "0xafe5539b40b17404e3cb6cf5013fc7dab3c54163", version: "" },

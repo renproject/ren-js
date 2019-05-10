679a5180b4c5faba57b6aca995397cd55f909249
noiach <gh@noiach.com>
2019-05-10 15:31:31 +1000

    Updated kovan addresses

--- packages/lib/networks/lib.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var mainnet = require("./public/networks/mainnet.json");
-var kovan = require("./public/networks/testnet.json");
-exports["default"] = {
-    mainnet: mainnet,
-    kovan: kovan
-};

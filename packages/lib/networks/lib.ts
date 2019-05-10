24f38ca59f82320a99e75aa5f948666cff1be953
noiach <gh@noiach.com>
2019-05-10 15:24:25 +1000

    Updated types

--- packages/lib/networks/lib.ts
+++ packages/lib/networks/lib.ts
@@ -1,7 +1,5 @@
-import * as mainnet from "./public/networks/mainnet.json";
-import * as kovan from "./public/networks/testnet.json";
+import * as mainnetJSON from "./public/networks/mainnet.json";
+import * as kovanJSON from "./public/networks/testnet.json";
 
-export default {
-    mainnet,
-    kovan,
-};
+export const mainnet = mainnetJSON;
+export const kovan = kovanJSON;

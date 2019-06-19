6f00e9f53b5695bdabbd407ce5bca607e9e6d30e
noiach <gh@noiach.com>
2019-06-19 11:07:03 +1000

    Added Contract class and hashCommitment

--- packages/lib/ren-js/ren-js/src/index.ts
+++ packages/lib/ren-js/ren-js/src/index.ts
@@ -1,4 +1,5 @@
 import Web3 from "web3";
+import { soliditySha3 } from "web3-utils";
 
 import { BitcoinUTXO, createBTCTestnetAddress, getBTCTestnetUTXOs } from "./blockchain/btc";
 import { createZECTestnetAddress, getZECTestnetUTXOs, ZcashUTXO } from "./blockchain/zec";
@@ -7,6 +8,11 @@ import {
 } from "./darknode/darknodeGroup";
 
 export type UTXO = { chain: Chain.Bitcoin, utxo: BitcoinUTXO } | { chain: Chain.ZCash, utxo: ZcashUTXO };
+
+export * from "./darknode/darknodeGroup";
+export * from "./blockchain/btc";
+export * from "./blockchain/zec";
+export * from "./blockchain/common";
 
 export enum Chain {
     Bitcoin = "btc",
@@ -77,5 +83,33 @@ export default class RenSDK {
     // Retrieves the current progress of the shift
     public shiftStatus = async (messageID: string): Promise<ShiftedInResponse | ShiftedOutResponse> => {
         return /*await*/ this.darknodeGroup.checkForResponse(messageID);
+    }
+
+    public hashCommitment = (commitment: any[], types: any[]): string => {
+        if (commitment.length !== types.length) {
+            throw new Error(`Commitment array and types array must have same length (length ${commitment.length} and ${types.length} respectively)`);
+        }
+
+        // You can annotate values passed in to soliditySha3.
+        // Example: { type: "address", value: srcToken }
+        const zip = commitment.map((value, i) => ({ type: types[i], value }));
+
+        return soliditySha3(...zip);
+    }
+}
+
+export class Contract {
+    private readonly sdk: RenSDK;
+    private readonly callName: string;
+    private readonly callTypes: string[];
+
+    constructor(sdk: RenSDK, callName: string, callTypes: string[]) {
+        this.sdk = sdk;
+        this.callName = callName;
+        this.callTypes = callTypes;
+    }
+
+    public hashCommitment = (values: any[]): string => {
+        return this.sdk.hashCommitment(values, this.callTypes);
     }
 }

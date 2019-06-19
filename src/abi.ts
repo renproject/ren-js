bf2506389614b846abd3d797bf26bece332618ae
noiach <gh@noiach.com>
2019-06-19 15:40:50 +1000

    Split index into multiple files

---
+++ src/abi.ts
@@ -0,0 +1,40 @@
+import { AbiItem } from "web3-utils";
+
+import { Payload } from "./utils";
+
+const shiftInABITemplate: AbiItem = {
+    constant: false,
+    inputs: [
+        {
+            name: "_amount",
+            type: "uint256"
+        },
+        {
+            name: "_nHash",
+            type: "bytes32"
+        },
+        {
+            name: "_sig",
+            type: "bytes"
+        },
+    ],
+    name: "shiftIn",
+    outputs: [],
+    payable: true,
+    stateMutability: "payable",
+    type: "function"
+};
+
+export const payloadToABI = (methodName: string, payload: Payload): AbiItem[] => {
+    return [
+        {
+            ...shiftInABITemplate,
+            name: methodName,
+            inputs: [
+                // tslint:disable-next-line: no-non-null-assertion
+                ...shiftInABITemplate.inputs!,
+                ...payload.map(value => ({ type: value.type, name: `${value.value}` }))
+            ]
+        }
+    ];
+};

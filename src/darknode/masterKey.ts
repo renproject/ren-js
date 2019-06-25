675dbdeca20f44fa880108f525f5f5a3173681b8
noiach <gh@noiach.com>
2019-06-25 16:19:24 +1000

    Updated testnet darknode addresses

--- src/darknode/masterKey.ts
+++ src/darknode/masterKey.ts
@@ -1,5 +1,6 @@
 
-const _testnetMasterPKH = Buffer.from("e02cabac3a62655335b1227dfdecfff27b5f6111", "hex");
+const _testnetMasterBtcPKH = Buffer.from("feea966136a436e44c96335455771943452728fc", "hex");
+const _testnetMasterEthPKH = Buffer.from("44Bb4eF43408072bC888Afd1a5986ba0Ce35Cb54", "hex");
 
 const _devnetMasterBtcPKH = Buffer.from("390e916c0f9022ef6cc44f05cd5094b2d9597574", "hex");
 const _devnetMasterEthPKH = Buffer.from("723eb4380e03df6a6f98cc1338b00cfbe5e45218", "hex");

ea854a4b4c6824e2471f72f7583498a837828d2b
noiach <gh@noiach.com>
2019-06-27 15:40:27 +1000

    Added test/template.spec.ts

---
+++ test/template.spec.ts
@@ -0,0 +1,21 @@
+import BigNumber from "bignumber.js";
+import chai from "chai";
+import chaiBigNumber from "chai-bignumber";
+
+require("dotenv").config();
+
+chai.use((chaiBigNumber)(BigNumber));
+chai.should();
+
+describe("Test template", function () {
+    // Disable test timeout.
+    this.timeout(0);
+
+    before(async () => {
+        // Setup before tests
+    });
+
+    it("", async () => {
+        // Run a test
+    });
+});

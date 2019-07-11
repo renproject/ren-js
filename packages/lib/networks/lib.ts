5583ae8f7cdc36d7cbab7ba1794245aab24ae6b7
noiach <gh@noiach.com>
2019-07-11 15:07:39 +1000

    Added DNR blocknumbers, exported RenNetwork enum

--- packages/lib/networks/lib.ts
+++ packages/lib/networks/lib.ts
@@ -7,3 +7,17 @@ export const mainnet = mainnetJSON;
 export const testnet = testnetJSON;
 export const devnet = devnetJSON;
 export const localnet = localnetJSON;
+
+export enum RenNetwork {
+    Mainnet = "mainnet",
+    Testnet = "testnet",
+    Devnet = "devnet",
+    Localnet = "localnet",
+}
+
+export const RenNetworks = {
+    [RenNetwork.Mainnet]: mainnet,
+    [RenNetwork.Testnet]: testnet,
+    [RenNetwork.Devnet]: devnet,
+    [RenNetwork.Localnet]: localnet,
+}

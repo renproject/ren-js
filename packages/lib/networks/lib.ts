602770c1e4a0b17f88ea36a76f5a27edb0dfa377
noiach <gh@noiach.com>
2019-07-11 11:24:30 +1000

    Added localnet

--- packages/lib/networks/lib.ts
+++ packages/lib/networks/lib.ts
@@ -1,7 +1,9 @@
 import * as devnetJSON from "./public/networks/devnet.json";
+import * as localnetJSON from "./public/networks/localnet.json";
 import * as mainnetJSON from "./public/networks/mainnet.json";
 import * as testnetJSON from "./public/networks/testnet.json";
 
 export const mainnet = mainnetJSON;
 export const testnet = testnetJSON;
 export const devnet = devnetJSON;
+export const localnet = localnetJSON;
--- packages/lib/networks/src/components/App.tsx
+++ packages/lib/networks/src/components/App.tsx
@@ -35,8 +35,9 @@ class App extends React.Component<AppProps, AppState> {
         const mainnet = (await axios.get(`./networks/mainnet.json?v=${Math.random().toString(36).substring(7)}`)).data;
         const testnet = (await axios.get(`./networks/testnet.json?v=${Math.random().toString(36).substring(7)}`)).data;
         const devnet = (await axios.get(`./networks/devnet.json?v=${Math.random().toString(36).substring(7)}`)).data;
+        const localnet = (await axios.get(`./networks/localnet.json?v=${Math.random().toString(36).substring(7)}`)).data;
         this.setState({
-            networks: OrderedMap({ mainnet, testnet, devnet })
+            networks: OrderedMap({ mainnet, testnet, devnet, localnet })
         });
 
         let using;

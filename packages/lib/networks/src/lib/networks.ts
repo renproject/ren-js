a1b48a1e34a8540aa3e67fdd22a92b09ef0290fb
noiach <gh@noiach.com>
2019-07-11 11:51:21 +1000

    Updated npm organization

--- packages/lib/networks/src/lib/networks.ts
+++ packages/lib/networks/src/lib/networks.ts
@@ -1,15 +1,3 @@
-export enum Category {
-    Republic = "republic",
-    RenEx = "renEx",
-    Tokens = "tokens",
-    Other = "other",
-}
-
-export interface ContractDetails {
-    address: string;
-    new?: true;
-}
-
 export interface NetworkData {
     name: string;
     chain: string;

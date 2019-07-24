2bdf2d6790564347cb3d598fe980d334d1a4cedd
Vince Au <vinceau09@gmail.com>
2019-07-24 15:26:51 +1000

    circleci: use the test bash script for all networks

--- .circleci/runtest.bash
+++ .circleci/runtest.bash
@@ -1,4 +1,4 @@
 yarn run test --reporter mocha-multi-reporters --reporter-options configFile=.circleci/mocha.json
 test_exit_code=$?
-echo "$test_exit_code" > devnet
+echo "$test_exit_code" > "$NETWORK"
 exit "$test_exit_code"

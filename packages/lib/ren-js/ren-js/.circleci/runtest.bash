1b35c3d5336d2c729ae052bb610da4c696f4587f
noiach <gh@noiach.com>
2019-07-24 05:03:09 +0000

    Merge pull request #7 from renproject/fix/test-exit-code
    Fix CircleCI devnet test exit code

--- packages/lib/ren-js/ren-js/.circleci/runtest.bash
+++ packages/lib/ren-js/ren-js/.circleci/runtest.bash
@@ -1,1 +1,4 @@
-yarn run test --reporter mocha-multi-reporters --reporter-options configFile=.circleci/mocha.json; echo $? > devnet || true
+yarn run test --reporter mocha-multi-reporters --reporter-options configFile=.circleci/mocha.json
+test_exit_code=$?
+echo "$test_exit_code" > devnet
+exit "$test_exit_code"

e5083dc9eeaea4f84ec5a9b761baf8bb177fc385
noiach <gh@noiach.com>
2019-11-04 18:46:54 +1100

    src: updated btc encoding format

--- packages/lib/ren-js/ren-js/test/blockchain/btcAndZec.spec.ts
+++ packages/lib/ren-js/ren-js/test/blockchain/btcAndZec.spec.ts
@@ -16,16 +16,20 @@ chai.should();
 describe("btc.ts", () => {
     describe("createBTCAddress", async () => {
         [
-            { network: NetworkMainnet, expected: "3GhPbsey6igoAf99Akhjq97pKgQkDSv9fA", get hex() { return Ox(decode(this.expected)); } },
-            { network: NetworkTestnet, expected: "2N4rtJpMggc72XKvHRQYoyyTKHkkPrCNEkv", get hex() { return Ox(decode(this.expected)); } },
-            { network: NetworkDevnet, expected: "2N29UCjbtgQPCa67d1qFPYciusV4w7yEwhn", get hex() { return Ox(decode(this.expected)); } },
-            { network: NetworkLocalnet, expected: "2NDzLJ2KMJYmFKw1HLs4v9Q98CpFCFieYaF", get hex() { return Ox(decode(this.expected)); } },
+            { network: NetworkMainnet, expected: "3GhPbsey6igoAf99Akhjq97pKgQkDSv9fA", get hex() { return Ox(Buffer.from(this.expected)); } },
+            { network: NetworkTestnet, expected: "2N4rtJpMggc72XKvHRQYoyyTKHkkPrCNEkv", get hex() { return Ox(Buffer.from(this.expected)); } },
+            { network: NetworkDevnet, expected: "2N29UCjbtgQPCa67d1qFPYciusV4w7yEwhn", get hex() { return Ox(Buffer.from(this.expected)); } },
+            { network: NetworkLocalnet, expected: "2NDzLJ2KMJYmFKw1HLs4v9Q98CpFCFieYaF", get hex() { return Ox(Buffer.from(this.expected)); } },
 
             // Segwit
-            { network: NetworkMainnet, actual: "bc1qc7slrfxkknqcq2jevvvkdgvrt8080852dfjewde450xdlk4ugp7szw5tk9", hex: "0x626331716337736c7266786b6b6e716371326a657676766b64677672743830383038353264666a6577646534353078646c6b3475677037737a7735746b39c8af1dac" },
-            { network: NetworkMainnet, actual: "bc1qar0srrr7xfkvy5l643lydnw9re59gtzzwf5mdq", hex: "0x62633171617230737272723778666b7679356c3634336c79646e77397265353967747a7a7766356d64710ead9977" },
-            { network: NetworkMainnet, actual: "bc1qw508d6qejxtdg4y5r3zarvary0c5xw7kv8f3t4", hex: "0x6263317177353038643671656a7874646734793572337a6172766172793063357877376b76386633743484fa1da3" },
-            { network: NetworkTestnet, actual: "tb1qw508d6qejxtdg4y5r3zarvary0c5xw7kxpjzsx", hex: "0x7462317177353038643671656a7874646734793572337a6172766172793063357877376b78706a7a7378fbdb81ce" },
+            // { network: NetworkMainnet, actual: "bc1qc7slrfxkknqcq2jevvvkdgvrt8080852dfjewde450xdlk4ugp7szw5tk9", hex: "0x626331716337736c7266786b6b6e716371326a657676766b64677672743830383038353264666a6577646534353078646c6b3475677037737a7735746b39c8af1dac" },
+            // { network: NetworkMainnet, actual: "bc1qar0srrr7xfkvy5l643lydnw9re59gtzzwf5mdq", hex: "0x62633171617230737272723778666b7679356c3634336c79646e77397265353967747a7a7766356d64710ead9977" },
+            // { network: NetworkMainnet, actual: "bc1qw508d6qejxtdg4y5r3zarvary0c5xw7kv8f3t4", hex: "0x6263317177353038643671656a7874646734793572337a6172766172793063357877376b76386633743484fa1da3" },
+            // { network: NetworkTestnet, actual: "tb1qw508d6qejxtdg4y5r3zarvary0c5xw7kxpjzsx", hex: "0x7462317177353038643671656a7874646734793572337a6172766172793063357877376b78706a7a7378fbdb81ce" },
+            { network: NetworkMainnet, actual: "bc1qc7slrfxkknqcq2jevvvkdgvrt8080852dfjewde450xdlk4ugp7szw5tk9", get hex() { return Ox(Buffer.from(this.actual)); } },
+            { network: NetworkMainnet, actual: "bc1qar0srrr7xfkvy5l643lydnw9re59gtzzwf5mdq", get hex() { return Ox(Buffer.from(this.actual)); } },
+            { network: NetworkMainnet, actual: "bc1qw508d6qejxtdg4y5r3zarvary0c5xw7kv8f3t4", get hex() { return Ox(Buffer.from(this.actual)); } },
+            { network: NetworkTestnet, actual: "tb1qw508d6qejxtdg4y5r3zarvary0c5xw7kxpjzsx", get hex() { return Ox(Buffer.from(this.actual)); } },
         ]
             .forEach(({ network, expected, hex, actual }) => {
                 it(network.name, async () => {

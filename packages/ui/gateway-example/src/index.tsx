e7482c960c0da16706c90100356fd6b05623bf08
noiach <gh@noiach.com>
2020-01-16 09:28:24 +1100

    src: added endpoint selector

--- packages/ui/gateway-example/src/index.tsx
+++ packages/ui/gateway-example/src/index.tsx
@@ -22,13 +22,20 @@ export enum ShiftInStatus {
     RefundedOnEthereum = "shiftIn_refundedOnEthereum",
 }
 
+enum Endpoints {
+    TESTNET = "testnet",
+    CHAOSNET = "chaosnet",
+    LOCALHOST = "http://localhost:3344/",
+}
+
 export const GatewayExample: React.FC<{}> = props => {
 
-    const gw = new GatewayJS("testnet");
+    const [endpoint, setEndpoint] = React.useState(Endpoints.TESTNET)
 
     // Called when the main button is pressed.
-    const startSwap = async () => {
+    const startSwap = React.useCallback(async () => {
         const amount = new BigNumber(0.000225);
+        const gw = new GatewayJS(endpoint);
         const response = await gw.open({
 
             // Send BTC from the Bitcoin blockchain to the Ethereum blockchain.
@@ -57,12 +64,13 @@ export const GatewayExample: React.FC<{}> = props => {
             ],
         });
         console.log(response);
-    };
+    }, [endpoint]);
 
     // Run once on load - check if there are any open trades that need to be
     // finished.
-    React.useEffect(() => {
+    const recoverTrades = React.useCallback(() => {
         (async () => {
+            const gw = new GatewayJS(endpoint);
             // TODO: export trade type
             // tslint:disable-next-line: no-any
             const unfinishedTrades: any[] = await gw.unfinishedTrades();
@@ -76,7 +84,7 @@ export const GatewayExample: React.FC<{}> = props => {
                 console.log(await responsePromise);
             }
         })().catch(error => console.error("Error in TestEnvironment.tsx: unfinishedTrades", error));
-    }, []);
+    }, [endpoint]);
 
     return (
         <div className="test-background">
@@ -84,13 +92,19 @@ export const GatewayExample: React.FC<{}> = props => {
                 <h1>Testing Environment</h1>
             </div></div>
             <div className="test-environment">
+                <div className="box">
+                    <p>To use this testing environment, you need to use a Web3 browser like Brave or Metamask for Firefox/Chrome.</p>
+                </div>
+                <select value={endpoint} onChange={e => setEndpoint(e.target.value as Endpoints)}>
+                    <option value={Endpoints.TESTNET}>Testnet</option>
+                    <option value={Endpoints.CHAOSNET}>Chaosnet</option>
+                    <option value={Endpoints.LOCALHOST}>Localhost</option>
+                </select>
                 <div>
-                    <div className="box">
-                        <p>To use this testing environment, you need to use a Web3 browser like Brave or Metamask for Firefox/Chrome.</p>
-                    </div>
+                    <button onClick={recoverTrades}>Recover trades</button>
                 </div>
                 <div>
-                    <button onClick={startSwap}>Open GatewayJS</button>
+                    <button className="blue" onClick={startSwap}>Open GatewayJS</button>
                 </div>
             </div>
         </div>

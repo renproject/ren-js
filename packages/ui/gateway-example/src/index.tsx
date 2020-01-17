5bfc157d973098fb117533ff9ae0541ef6711f91
noiach <gh@noiach.com>
2020-01-17 16:22:49 +1100

    package.json,src: updated gateway-js

--- packages/ui/gateway-example/src/index.tsx
+++ packages/ui/gateway-example/src/index.tsx
@@ -2,10 +2,9 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 import GatewayJS, { HistoryEvent } from "@renproject/gateway-js";
-// TODO: Don't require RenJS to be imported
-import RenJS from "@renproject/ren";
 import BigNumber from "bignumber.js";
 
+// TODO: Don't require RenJS to be imported
 import "./style.scss";
 
 const contractAddress = "0xa2aE9111634F5983e4e1C3E3823914841a4c7235";
@@ -38,13 +37,13 @@ export const GatewayExample: React.FC<{}> = props => {
     // Called when the main button is pressed.
     const startSwap = React.useCallback(async () => {
         setOpening(true);
-        const amount = new BigNumber(0.000225);
+        const amount = new BigNumber(0.000225); // BTC
         const gw = new GatewayJS(endpoint);
-        const response = await gw.open({
+        await gw.open({
 
             // Send BTC from the Bitcoin blockchain to the Ethereum blockchain.
             // TODO: Expose from GatewayJS
-            sendToken: RenJS.Tokens.BTC.Btc2Eth,
+            sendToken: GatewayJS.Tokens.BTC.Btc2Eth,
 
             // Amount of BTC we are sending (in Satoshis)
             sendAmount: amount.times(10 ** 8).toNumber(), // Convert to Satoshis
@@ -56,7 +55,7 @@ export const GatewayExample: React.FC<{}> = props => {
             contractFn: "shiftIn",
 
             // TODO: Expose from GatewayJS
-            nonce: RenJS.utils.randomNonce(),
+            nonce: GatewayJS.utils.randomNonce(),
 
             // Arguments expected for calling `deposit`
             contractParams: [
@@ -81,8 +80,8 @@ export const GatewayExample: React.FC<{}> = props => {
             const gw = new GatewayJS(endpoint);
             // TODO: export trade type
             // tslint:disable-next-line: no-any
-            const unfinishedTrades: Map<string, HistoryEvent> = await gw.unfinishedTrades();
-            for (const trade of Array.from(unfinishedTrades.values())) {
+            const getGateways: Map<string, HistoryEvent> = await gw.getGateways();
+            for (const trade of Array.from(getGateways.values())) {
                 if (trade.status === ShiftInStatus.ConfirmedOnEthereum) {
                     continue;
                 }
@@ -94,7 +93,7 @@ export const GatewayExample: React.FC<{}> = props => {
                     .catch(error => { console.error(error); setError(error.message) });
                 // await gw.cancel();
             }
-        })().catch(error => { console.error("Error in TestEnvironment.tsx: unfinishedTrades", error); setError(error.message) });
+        })().catch(error => { console.error("Error in TestEnvironment.tsx: getGateways", error); setError(error.message) });
     }, [endpoint]);
 
     return (

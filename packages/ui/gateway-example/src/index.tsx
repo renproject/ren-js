7bce8a66b1a312f0784f7d1d5687528327b6d177
noiach <gh@noiach.com>
2020-01-22 16:51:48 +1100

    src: updated @renproject/gateway-js

--- packages/ui/gateway-example/src/index.tsx
+++ packages/ui/gateway-example/src/index.tsx
@@ -6,9 +6,9 @@ import BigNumber from "bignumber.js";
 
 // TODO: Don't require RenJS to be imported
 import "./style.scss";
+import { getWeb3 } from "./web3";
 
 const contractAddress = "0xa2aE9111634F5983e4e1C3E3823914841a4c7235";
-const address = "0xa2aE9111634F5983e4e1C3E3823914841a4c7235";
 
 // TODO: Export from gateway
 export enum ShiftInStatus {
@@ -35,11 +35,20 @@ export const GatewayExample: React.FC<{}> = props => {
     const [error, setError] = React.useState(null as string | null);
 
     // Called when the main button is pressed.
-    const startSwap = React.useCallback(async () => {
+    const startShiftIn = React.useCallback(async () => {
         setOpening(true);
         setError(null);
         const amount = new BigNumber(0.000225); // BTC
         const gw = new GatewayJS(endpoint);
+
+        const web3 = await getWeb3();
+        const accounts = await web3.eth.getAccounts();
+        if (!accounts || !accounts.length) {
+            setError("Must be logged in to a Web3 browser.");
+            setOpening(false);
+            return;
+        }
+
         gw.open({
 
             // Send BTC from the Bitcoin blockchain to the Ethereum blockchain.
@@ -62,7 +71,7 @@ export const GatewayExample: React.FC<{}> = props => {
                 {
                     name: "_to",
                     type: "address",
-                    value: address,
+                    value: accounts[0],
                 }
             ],
         })
@@ -71,6 +80,49 @@ export const GatewayExample: React.FC<{}> = props => {
             .then(result => { console.log(result); setOpening(false); })
             .catch(error => { setOpening(false); console.error(error); setError(error.message) });
     }, [endpoint]);
+
+    // const startShiftOut = React.useCallback(async () => {
+    //     setOpening(true);
+    //     setError(null);
+    //     const amount = new BigNumber(0.000225); // BTC
+    //     const gw = new GatewayJS(endpoint);
+
+    //     const web3 = await getWeb3();
+
+    //     gw.open({
+
+    //         // Send BTC from the Ethereum blockchain to the Bitcoin blockchain.
+    //         // This is the reverse of shitIn.
+    //         sendToken: GatewayJS.Tokens.BTC.Eth2Btc,
+
+    //         // The web3 provider to talk to Ethereum
+    //         web3Provider: web3.currentProvider,
+
+    //         // The contract we want to interact with
+    //         sendTo: contractAddress,
+
+    //         // The name of the function we want to call
+    //         contractFn: "shiftOut",
+
+    //         // Arguments expected for calling `deposit`
+    //         contractParams: [
+    //             {
+    //                 name: "_to",
+    //                 type: "bytes",
+    //                 value: address,
+    //             },
+    //             {
+    //                 name: "_amount",
+    //                 type: "uint256",
+    //                 value: amount,
+    //             }
+    //         ],
+    //     })
+    //         .result()
+    //         .on("status", (status, details) => console.log(`[GOT STATUS] ${status} (${details})`))
+    //         .then(result => { console.log(result); setOpening(false); })
+    //         .catch(error => { setOpening(false); console.error(error); setError(error.message) });
+    // }, [endpoint]);
 
     // Run once on load - check if there are any open trades that need to be
     // finished.
@@ -86,7 +138,7 @@ export const GatewayExample: React.FC<{}> = props => {
                 if (trade.status === ShiftInStatus.ConfirmedOnEthereum) {
                     continue;
                 }
-                const gw = new GatewayJS(endpoint).open(trade.commitment);
+                const gw = new GatewayJS(endpoint).open(trade);
                 gw.pause();
                 (window as any).gw = gw;
                 gw
@@ -116,7 +168,7 @@ export const GatewayExample: React.FC<{}> = props => {
                     <button disabled={recovering} onClick={recoverTrades}>Recover trades</button>
                 </div>
                 <div>
-                    <button className="blue" onClick={startSwap}>Open GatewayJS</button>
+                    <button className="blue" onClick={startShiftIn}>Open GatewayJS</button>
                 </div>
                 {error ? <div className="box red">{error}</div> : <></>}
             </div>

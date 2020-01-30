ec6e5647d0d19315d69f1787d2c2dcbb3a8e103c
noiach <gh@noiach.com>
2020-01-31 02:57:32 +1100

    src: simplified example

--- packages/ui/gateway-example/src/index.tsx
+++ packages/ui/gateway-example/src/index.tsx
@@ -1,180 +1,71 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import GatewayJS, { HistoryEvent } from "@renproject/gateway-js";
-import BigNumber from "bignumber.js";
+import GatewayJS, { ShiftInStatus, ShiftOutStatus } from "@renproject/gateway-js";
 
-// TODO: Don't require RenJS to be imported
 import "./style.scss";
-import { getWeb3 } from "./web3";
 
-const contractAddress = "0xa2aE9111634F5983e4e1C3E3823914841a4c7235";
+const gatewayJS = new GatewayJS("testnet");
 
-// TODO: Export from gateway
-export enum ShiftInStatus {
-    Committed = "shiftIn_committed",
-    Deposited = "shiftIn_deposited",
-    SubmittedToRenVM = "shiftIn_submittedToRenVM",
-    ReturnedFromRenVM = "shiftIn_returnedFromRenVM",
-    SubmittedToEthereum = "shiftIn_submittedToEthereum",
-    ConfirmedOnEthereum = "shiftIn_confirmedOnEthereum",
-    RefundedOnEthereum = "shiftIn_refundedOnEthereum",
-}
+const startShiftIn = async () => {
+    const amount = 0.000225; // BTC
+    const recipient = prompt("Enter Ethereum address to receive BTC");
+    if (!recipient) { throw new Error(`No recipient provided. `) };
 
-enum Endpoints {
-    TESTNET = "testnet",
-    CHAOSNET = "chaosnet",
-    LOCALHOST = "http://localhost:3344/",
-}
+    gatewayJS.open({
+        // Send BTC from the Bitcoin blockchain to the Ethereum blockchain.
+        sendToken: GatewayJS.Tokens.BTC.Btc2Eth,
 
-export const GatewayExample: React.FC<{}> = props => {
+        // Amount of BTC we are sending (in Satoshis)
+        sendAmount: GatewayJS.utils.value(amount, "btc").sats(),
 
-    const [endpoint, setEndpoint] = React.useState(Endpoints.TESTNET)
-    const [recovering, setRecovering] = React.useState(false);
-    const [opening, setOpening] = React.useState(false);
-    const [error, setError] = React.useState(null as string | null);
+        // The contract we want to interact with
+        sendTo: "0xa2aE9111634F5983e4e1C3E3823914841a4c7235",
 
-    // Called when the main button is pressed.
-    const startShiftIn = React.useCallback(async () => {
-        setOpening(true);
-        setError(null);
-        const amount = new BigNumber(0.000225); // BTC
-        const gw = new GatewayJS(endpoint);
+        // The name of the function we want to call
+        contractFn: "shiftIn",
 
-        const web3 = await getWeb3();
-        const accounts = await web3.eth.getAccounts();
-        if (!accounts || !accounts.length) {
-            setError("Must be logged in to a Web3 browser.");
-            setOpening(false);
-            return;
-        }
-
-        gw.open({
-
-            // Send BTC from the Bitcoin blockchain to the Ethereum blockchain.
-            sendToken: GatewayJS.Tokens.BTC.Btc2Eth,
-
-            // Amount of BTC we are sending (in Satoshis)
-            sendAmount: amount.times(10 ** 8).toNumber(), // Convert to Satoshis
-
-            // The contract we want to interact with
-            sendTo: contractAddress,
-
-            // The name of the function we want to call
-            contractFn: "shiftIn",
-
-            // The nonce is used to guarantee a unique deposit address.
-            nonce: GatewayJS.utils.randomNonce(),
-
-            // Arguments expected for calling `deposit`
-            contractParams: [
-                {
-                    name: "_to",
-                    type: "address",
-                    value: accounts[0],
-                }
-            ],
-        })
-            .result()
-            .on("status", (status, details) => console.log(`[GOT STATUS] ${status} (${details})`))
-            .then(result => { console.log(result); setOpening(false); })
-            .catch(error => { setOpening(false); console.error(error); setError(error.message) });
-    }, [endpoint]);
-
-    // const startShiftOut = React.useCallback(async () => {
-    //     setOpening(true);
-    //     setError(null);
-    //     const amount = new BigNumber(0.000225); // BTC
-    //     const gw = new GatewayJS(endpoint);
-
-    //     const web3 = await getWeb3();
-
-    //     gw.open({
-
-    //         // Send BTC from the Ethereum blockchain to the Bitcoin blockchain.
-    //         // This is the reverse of shitIn.
-    //         sendToken: GatewayJS.Tokens.BTC.Eth2Btc,
-
-    //         // The web3 provider to talk to Ethereum
-    //         web3Provider: web3.currentProvider,
-
-    //         // The contract we want to interact with
-    //         sendTo: contractAddress,
-
-    //         // The name of the function we want to call
-    //         contractFn: "shiftOut",
-
-    //         // Arguments expected for calling `deposit`
-    //         contractParams: [
-    //             {
-    //                 name: "_to",
-    //                 type: "bytes",
-    //                 value: address,
-    //             },
-    //             {
-    //                 name: "_amount",
-    //                 type: "uint256",
-    //                 value: amount,
-    //             }
-    //         ],
-    //     })
-    //         .result()
-    //         .on("status", (status, details) => console.log(`[GOT STATUS] ${status} (${details})`))
-    //         .then(result => { console.log(result); setOpening(false); })
-    //         .catch(error => { setOpening(false); console.error(error); setError(error.message) });
-    // }, [endpoint]);
-
-    // Run once on load - check if there are any open trades that need to be
-    // finished.
-    const recoverTrades = React.useCallback(() => {
-        setRecovering(true);
-        setError(null);
-        (async () => {
-            const gw = new GatewayJS(endpoint);
-            // TODO: export trade type
-            // tslint:disable-next-line: no-any
-            const getGateways: Map<string, HistoryEvent> = await gw.getGateways();
-            for (const trade of Array.from(getGateways.values())) {
-                if (trade.status === ShiftInStatus.ConfirmedOnEthereum) {
-                    continue;
-                }
-                const gw = new GatewayJS(endpoint).open(trade);
-                gw.pause();
-                (window as any).gw = gw;
-                gw
-                    .result()
-                    .on("status", (status, details) => console.log(`[GOT STATUS] ${status} (${details})`))
-                    .then(result => { console.log(result); })
-                    .catch(error => { console.error(error); setError(error.message); });
+        // Arguments expected for calling `deposit`
+        contractParams: [
+            {
+                name: "_to",
+                type: "address",
+                value: recipient,
             }
-        })().catch(error => { console.error("Error in TestEnvironment.tsx: getGateways", error); setError(error.message) });
-    }, [endpoint]);
+        ],
 
-    return (
-        <div className="test-background">
-            <div className="test-banner"><div className="container">
-                <h1>Testing Environment</h1>
-            </div></div>
-            <div className="test-environment">
-                <div className="box">
-                    <p>To use this testing environment, you need to use a Web3 browser like Brave or Metamask for Firefox/Chrome.</p>
-                </div>
-                <select disabled={opening || recovering} value={endpoint} onChange={e => setEndpoint(e.target.value as Endpoints)}>
-                    <option value={Endpoints.TESTNET}>Testnet</option>
-                    <option value={Endpoints.CHAOSNET}>Chaosnet</option>
-                    <option value={Endpoints.LOCALHOST}>Localhost</option>
-                </select>
-                <div>
-                    <button disabled={recovering} onClick={recoverTrades}>Recover trades</button>
-                </div>
-                <div>
-                    <button className="blue" onClick={startShiftIn}>Open GatewayJS</button>
-                </div>
-                {error ? <div className="box red">{error}</div> : <></>}
-            </div>
-        </div>
-    );
+        // The nonce is used to guarantee a unique deposit address.
+        nonce: GatewayJS.utils.randomNonce(),
+    }).result()
+        .on("status", (status) => console.log(`[GOT STATUS] ${status}`))
+        .then(console.log)
+        .catch(console.error);
 };
+
+const recoverTrades = async () => {
+    // Re-open incomplete trades
+    const previousGateways = await gatewayJS.getGateways();
+    for (const trade of Array.from(previousGateways.values())) {
+        if (trade.status === ShiftInStatus.ConfirmedOnEthereum || trade.status === ShiftOutStatus.ReturnedFromRenVM) { continue; }
+        const gateway = gatewayJS.open(trade);
+        gateway.pause();
+        gateway.result()
+            .on("status", (status) => console.log(`[GOT STATUS] ${status}`))
+            .then(console.log)
+            .catch(console.error);
+    }
+}
+
+const GatewayExample = () => {
+    React.useEffect(() => { recoverTrades(); }, []);
+    return <div className="test-background">
+        <div className="test-banner"><div className="container"><h1>Testing Environment</h1></div></div>
+        <div className="test-environment">
+            <p className="box">To use this testing environment, you need to use a Web3 browser like Brave or Metamask for Firefox/Chrome.</p>
+            <button onClick={startShiftIn} className="blue">Shift in with GatewayJS</button>
+        </div>
+    </div>;
+}
 
 
 ReactDOM.render(<GatewayExample />, document.getElementById("root") as HTMLElement);

dc4921da58d07c2d61b1c879ca3140a2cc70b9e9
noiach <gh@noiach.com>
2020-04-08 22:20:42 +1000

    src: fixed localnet using old nomeclature

--- packages/lib/networks/src/networks/localnet.ts
+++ packages/lib/networks/src/networks/localnet.ts
@@ -73,7 +73,7 @@ export default Network({
                 artifact: DarknodePaymentStore,
             },
         },
-        shifter: {
+        gateways: {
             GatewayRegistry: {
                 address: GatewayRegistry.networks[networkID].address,
                 abi: GatewayRegistry.abi as AbiItem[],

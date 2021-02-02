3aa7e55b6acbe91b44963f3f476cc1ff625c6dbd
noiach <gh@noiach.com>
2021-02-02 15:47:59 +1100

    feat: increase ren-tx test timeouts

--- packages/lib/ren-tx/test/mint.spec.ts
+++ packages/lib/ren-tx/test/mint.spec.ts
@@ -35,7 +35,7 @@ const makeMintTransaction = (): GatewaySession => ({
     customParams: {},
 });
 
-jest.setTimeout(1000 * 46);
+jest.setTimeout(1000 * 200);
 describe("MintMachine", () => {
     it("should create a tx", async () => {
         const fromChainMap = {

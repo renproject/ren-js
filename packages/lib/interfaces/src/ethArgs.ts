80db972aa49cea1684ea384f3cf5411d5abb2148
noiach <gh@noiach.com>
2021-02-05 12:29:36 +1100

    feat: updated comments for notInPayload and onlyInPayload

--- packages/lib/interfaces/src/ethArgs.ts
+++ packages/lib/interfaces/src/ethArgs.ts
@@ -119,7 +119,27 @@ export interface EthArg<
     name: name;
     type: type;
     value: valueType;
+
+    /**
+     * `notInPayload` indicates that the parameter should be used when calling
+     * the smart contract but it should not be used when calculating the
+     * payload hash. This is useful for values can only be known at the time
+     * of calling the contract. Note that others may be able to submit the mint
+     * and set their own value, unless the contract caller is restricted somehow.
+     */
     notInPayload?: boolean;
+
+    /**
+     * `onlyInPayload` indicates that the parameter should be used when
+     * calculating the payload hash but it should not be passed in to the
+     * contract call. This is useful for values that don't need to be explicitly
+     * passed in to the contract, such as the contract caller.
+     *
+     * `notInPayload` and `onlyInPayload` can be used together to allow users to
+     * update values such as exchange rate slippage, which may have updated
+     * while waiting for the lock-chain confirmations - while ensuring that
+     * others can't change it for them.
+     */
     onlyInPayload?: boolean;
 }
 

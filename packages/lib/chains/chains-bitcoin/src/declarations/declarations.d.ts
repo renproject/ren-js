2990940a6ca1f450fa887ebd1ede70d59b8956bf
noiach <gh@noiach.com>
2021-03-03 17:10:14 +1100

    feat(ren-js): update digibyte address validation

--- packages/lib/chains/chains-bitcoin/src/declarations/declarations.d.ts
+++ packages/lib/chains/chains-bitcoin/src/declarations/declarations.d.ts
@@ -1,4 +1,5 @@
 declare module "wallet-address-validator";
 declare module "bitcore-lib/lib/encoding/base58check";
 declare module "bitcore-lib-zcash/lib/encoding/base58check";
+declare module "wallet-address-validator/src/bitcoin_validator";
 declare module "bs58check";
--- packages/lib/chains/chains-bitcoin/src/utils.ts
+++ packages/lib/chains/chains-bitcoin/src/utils.ts
@@ -1,7 +1,23 @@
 import { validate } from "wallet-address-validator";
 
+import BTCValidator from "wallet-address-validator/src/bitcoin_validator";
+
 export const validateAddress = (
     address: string,
     asset: string,
     network: string,
-) => validate(address, asset, network === "mainnet" ? "prod" : network);
+) => {
+    if (asset === "DGB") {
+        const currency = {
+            name: "digibyte",
+            symbol: "dgb",
+            addressTypes: { prod: ["1e", "3f"], testnet: ["7e", "8c"] },
+            validator: BTCValidator,
+            segwitHrp: network === "mainnet" ? "dgb" : "dgbt",
+        };
+
+        return currency.validator.isValidAddress(address, currency, network);
+    }
+
+    return validate(address, asset, network === "mainnet" ? "prod" : network);
+};

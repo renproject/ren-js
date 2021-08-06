be177aa9a47916e5e1b5d2ec83a41e4c432ea92f
Ryan <serjndestroy@gmail.com>
2021-08-06 11:11:30 +0800

    fix(lint): fix config per https://github.com/prettier/eslint-config-prettier/blob/main/CHANGELOG.md#version-800-2021-02-21

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,6 @@ module.exports = {
         "plugin:@typescript-eslint/recommended",
         "plugin:@typescript-eslint/recommended-requiring-type-checking",
         "prettier",
-        "prettier/@typescript-eslint",
 
         // Ignored for now.
         // "plugin:security/recommended",

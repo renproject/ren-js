488345fd6403572509ac0626a13c652c2e533d05
noiach <gh@noiach.com>
2022-06-30 16:50:22 +1000

    chore(chains-ethereum): fix lint errors

--- packages/chains/chains-ethereum/src/index.ts
+++ packages/chains/chains-ethereum/src/index.ts
@@ -11,9 +11,9 @@ export {
 // Chains
 export * from "./arbitrum";
 export * from "./avalanche";
+export * from "./bsc";
+export * from "./catalog";
+export * from "./ethereum";
 export * from "./fantom";
 export * from "./goerli";
 export * from "./polygon";
-export * from "./bsc";
-export * from "./ethereum";
-export * from "./catalog";
